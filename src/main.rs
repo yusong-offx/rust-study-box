040a213142b7680f66ca5a4df5c3ad28d8779b61
yj.song <hssong1024@gmail.com>
2024-01-24 07:27:26 +0000

    rust xml study; using

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,42 @@
+use quick_xml::events::{BytesEnd, BytesStart, Event};
+use quick_xml::reader::Reader;
+use quick_xml::writer::Writer;
+use std::io::Cursor;
+
 fn main() {
-    println!("Hello, world!");
+    let xml = r#"<this_tag k1="v1" k2="v2"><child>text</child></this_tag>"#;
+    let mut reader = Reader::from_str(xml);
+    reader.trim_text(true);
+    let mut writer = Writer::new(Cursor::new(Vec::new()));
+    loop {
+        match reader.read_event() {
+            Ok(Event::Start(e)) if e.name().as_ref() == b"this_tag" => {
+                // crates a new element ... alternatively we could reuse `e` by calling
+                // `e.into_owned()`
+                let mut elem = BytesStart::new("my_elem");
+
+                // collect existing attributes
+                elem.extend_attributes(e.attributes().map(|attr| attr.unwrap()));
+
+                // copy existing attributes, adds a new my-key="some value" attribute
+                elem.push_attribute(("my-key", "some value"));
+
+                // writes the event to the writer
+                assert!(writer.write_event(Event::Start(elem)).is_ok());
+            }
+            Ok(Event::End(e)) if e.name().as_ref() == b"this_tag" => {
+                assert!(writer
+                    .write_event(Event::End(BytesEnd::new("my_elem")))
+                    .is_ok());
+            }
+            Ok(Event::Eof) => break,
+            // we can either move or borrow the event to write, depending on your use-case
+            Ok(e) => assert!(writer.write_event(e).is_ok()),
+            Err(e) => panic!("Error at position {}: {:?}", reader.buffer_position(), e),
+        }
+    }
+
+    let result = writer.into_inner().into_inner();
+    let expected = r#"<my_elem k1="v1" k2="v2" my-key="some value"><child>text</child></my_elem>"#;
+    assert_eq!(result, expected.as_bytes());
 }
